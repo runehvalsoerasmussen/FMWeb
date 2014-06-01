692c159515354f2534df31d683a2401f8408bd86
runehvalsoerasmussen <rune@danskerne.se>
2014-06-01 21:33:29 +0200

    Fix a bug

--- read_start.php
+++ read_start.php
@@ -60,6 +60,7 @@ if (isset($_POST["testKode"],
       } catch (PDOException $e) {
          printf("<br>Huston 2 we had a problem: %s\n",$e->getMessage());
          echo("</body></html>\n");
+         exit();
       }
    if ($foundTest == 1)
    {
@@ -76,187 +77,15 @@ if (isset($_POST["testKode"],
          $QSvar=$row['QSvar'];
          $TotalPages=$row['TotalID'];
          $PictName=$row['Pict'];
-         write_edit_form($CurrentTestPage, $TotalPages, $TestID, $ExplainText, $PictName, $QuestionText, $QSvar, $CorrectAnswers, $Q1Text, $Q2Text, $Q3Text);
+         write_static_form($CurrentTestPage, $TotalPages, $TestID, $ExplainText, $PictName, $QuestionText, $QSvar, $CorrectAnswers, $Q1Text, $Q2Text, $Q3Text);
       } catch (PDOException $e) {
          printf("<br>Huston 2 we had a problem: %s\n",$e->getMessage());
          echo("</body></html>\n");
+         exit();
       }
 
    }
-        <!-- ydre ramme -->
-    <div id="frame1"><!-- ydre ramme -->
 
-        <!-- Logo banner i toppen -->
-    <div id="logo"></div>
-
-        <!-- Ramme rundt tekstindhold -->
-    <div id="frame2"><p></p></div>
-
-        <!-- Indholdstekst/ den forklarende tekst -->
-    <div id="frame2a"><p><span style="font-size:1.4em; color: #9F000F; font-style:italic "> Skriv overskrift her</span><br><br> Skriv indhold her
-
-        </p></div>
-
-        <!-- Billede - box -->
-    <div id="frame3"><img src="Loven2.jpg"  alt=""  /></div>
-
-
-        <!-- TEXT - Næste/ box -->
-    <div id="frame3a">
-        <a href="#" class="css_btn_class"><span style="font-size:1.2em; color: #9F000F; font-style:italic "> Næste Spørgsmål </span></a></div>
-
-
-        <!-- Overskrift /Spørgsmål -->
-    <div id="frame4"><span style="font-size:1.4em; color: #9F000F; font-style:italic "> Overskift på spørgsmål
- </span></div>
-
-        <!-- Spørgsmål 1 -->
-    <div id="frame4a">Spørgsmål 1</div>
-
-
-        <!-- Spørgsmål 2 -->
-    <div id="frame4b">Spørgsmål 2</div>
-
-
-
-        <!-- Spørgsmål 3 -->
-    <div id="frame4c">Spørgsmål 3</div>
-
-
-
-    <section title=".squaredFour">
-    <!-- Checkbox 1 -->
-    <div class="squaredFour">
-      <input type="checkbox" value="None" id="squaredFour" name="check" checked />
-      <label for="squaredFour"></label>
-    </div>
-    <!-- end Checkbox 1 -->
-  </section>
-
-     <section title=".squaredFour1">
-    <!-- Checkbox 2 -->
-    <div class="squaredFour1">
-      <input type="checkbox" value="None" id="squaredFour1" name="check" checked />
-      <label for="squaredFour1"></label>
-    </div>
-    <!-- end Checkbox 2 -->
-  </section>
-
- <section title=".squaredFour2">
-    <!-- Checkbox 3 -->
-    <div class="squaredFour2">
-      <input type="checkbox" value="None" id="squaredFour2" name="check" checked />
-      <label for="squaredFour2"></label>
-    </div>
-    <!-- end Checkbox 3 -->
-  </section>
-
-
-<?php
-   echo "<p>Welcome\n";
-   echo htmlentities($_SESSION['username']);
-   echo "!</p>\n";
-   $CorrectAnswers = 0;
-   $UserID = $_POST["UserID"];
-   $TestID = $_POST["TestID"];
-   $CurrentTestPage = $_POST["PageID"];
-   $CorrectAnswers = $_POST["CorrectAnswers"];
-
-   $ExplainText="";
-   $QuestionText="";
-   $Q1Text="";
-   $Q2Text="";
-   $Q3Text="";
-   $QSvar=0;
-   $NextPage=0;
-   $TotalPages = 0;
-   $PictName="";
-
-   if ($CurrentTestPage == 0) {
-      try{
-         $db = new PDO('mysql:host=danskerne.se.mysql;dbname=danskerne_se', 'danskerne_se', $password);
-         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-         $statement = $db->query("SELECT * FROM UdTest WHERE TestID=$TestID");
-         $row = $statement->fetch(PDO::FETCH_ASSOC);
-         $TotalPages=$row['TotalID'];
-      }
-      catch (PDOException $e) {
-         printf("<br>Huston 1 we had a problem: %s\n",$e->getMessage());
-      }
-   printf("<h1>Fantastisk - Du er faerdig, du fik %s rigtige svar ud af %s</h1>\n", $CorrectAnswers, $TotalPages);
-   } else {
-      try{
-         $db = new PDO('mysql:host=danskerne.se.mysql;dbname=danskerne_se', 'danskerne_se', $password);
-         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-         $statement = $db->query("SELECT * FROM UdTest WHERE TestID=$TestID AND PageID=$CurrentTestPage");
-         $row = $statement->fetch(PDO::FETCH_ASSOC);
-         $ExplainText=$row['ExplainTXT'];
-         $QuestionText=$row['QuestionTXT'];
-         $Q1Text=$row['Q1'];
-         $Q2Text=$row['Q2'];
-         $Q3Text=$row['Q3'];
-         $QSvar=$row['QSvar'];
-         $TotalPages=$row['TotalID'];
-         $PictName=$row['Pict'];
-         }
-         catch (PDOException $e) {
-            printf("<br>Huston 2 we had a problem: %s\n",$e->getMessage());
-         }
-      echo "<div id=\"frame1\">\n";
-      echo "<form action=\"readnext.php\" method=\"post\">\n";
-      if ($CurrentTestPage < $TotalPages) {
-         printf("<INPUT type=\"hidden\" name=\"PageID\" value=\"%s\">\n", $CurrentTestPage+1);
-      } else {
-         if ($CurrentTestPage == $TotalPages) {
-            printf("<INPUT type=\"hidden\" name=\"PageID\" value=\"%s\">\n", 0);
-         }
-      }
-      printf("<INPUT type=\"hidden\" name=\"UserID\" value=\"%s\">\n", $UserID);
-      printf("<INPUT type=\"hidden\" name=\"TestID\" value=\"%s\">\n", $TestID);
-      printf("<p>Side %s af totalt %s\n", $CurrentTestPage, $TotalPages);
-      echo "<div id=\"frame2\">\n";
-      echo htmlentities($ExplainText);
-      echo "</div>\n";
-      echo "<div id=\"frame3\">\n";
-      printf("<img src=\"..\/%s\" width=\"%s\" height=\"%s\" />\n", $PictName, "100%", "100%");
-      echo "</div>\n";
-      echo "<div id=\"frame3b\">\n";
-      echo htmlentities($QuestionText);
-      echo "</div>\n";
-      echo "<div id=\"frame4\">\n";
-      echo "<input type=\"radio\" onclick=\"enableNext()\" name=\"CorrectAnswers\"";
-      if ($QSvar == 1){
-         printf("value=\"%s\">\n", ($CorrectAnswers + 1));
-      } else {
-         printf("value=\"%s\">\n", $CorrectAnswers);
-      }
-      echo htmlentities($Q1Text);
-      echo "</div>\n";
-      echo "<div id=\"frame5\">\n";
-      echo "<input type=\"radio\" onclick=\"enableNext()\" name=\"CorrectAnswers\"";
-      if ($QSvar == 2){
-         printf("value=\"%s\">\n", ($CorrectAnswers + 1));
-      } else {
-         printf("value=\"%s\">\n", $CorrectAnswers);
-      }
-      echo htmlentities($Q2Text);
-      echo "<br>\n";
-      echo "</div>\n";
-      echo "<div id=\"frame6\">\n";
-      echo "<input type=\"radio\" onclick=\"enableNext()\" name=\"CorrectAnswers\"";
-      if ($QSvar == 3){
-         printf("value=\"%s\">\n", ($CorrectAnswers + 1));
-      } else {
-         printf("value=\"%s\">\n", $CorrectAnswers);
-      }
-      echo htmlentities($Q3Text);
-      echo "<br>\n";
-      echo "</div>\n";
-      echo "<div id=\"frame7\">\n";
-      echo "</div>\n";
-      echo "</form>\n";
-      echo "</div>\n";
-   }
 
 ?>
 
